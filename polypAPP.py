a72b2a6d9614e01ea12adf299699e89683ba52d7
MoizAhmed2517 <111642632+MoizAhmed2517@users.noreply.github.com>
2023-01-12 23:38:32 +0500

    Updated "1/12/2023"

--- polypAPP.py
+++ polypAPP.py
@@ -9,48 +9,68 @@ hide_st_style = """
     </style>
 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-#
-# selected = option_menu(
-#     menu_title=None,
-#     options=["Polyp AI", "predict"],
-#     default_index=0,
-#     orientation="horizontal",
-#     styles={
-#         "container": {"padding": "0!important", "background-color": "#fafafa"},
-#         "icon": {"color": "orange", "font-size": "25px"},
-#         "nav-link": {"font-size": "25px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
-#         "nav-link-selected": {"background-color": "#0f386d"},
-#     }
-# )
-#
-# # st.title('Polyp Detection App')
-#
-# container = st.container()
-# container.write("So what Can i used instead of box?")
 
-# [theme]
-# base="light"
-# primaryColor="#0f386d"
 
 # __________________ CREATING NAVBAR ____________________
 
 st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown("""
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #0A1929;">
-        <span class="navbar-brand h1 mx-2 fs-3" style="margin-bottom: -5px; font-weight: 500px;">Polyp<span class="navbar-brand h1 mx-2 fs-3">AI</span></span>
-        
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #0B233C;">
+        <span class="navbar-brand h1 mx-3 fs-3" style="margin-bottom: -5px; font-weight: 500px; margin-top: -5px;">Polyp AI</span>
     </nav>
 """, unsafe_allow_html=True)
 
-container = st.container()
+# __________________ CREATING MAIN HEADING ____________________
 
-container.markdown("""
-    <div class="container" style="background-color: #0A1929; border-radius: 10px; margin-top: -35px;">
-    <h3 class="h1 text-center">Polyp Detector</h3>
-    </div>
-""", unsafe_allow_html=True)
+st.title("Polyp Detector")
+title_styling="""
+<style>
+#polyp-detector {
+  text-align: center;
+  font-size: 30px;
+  font-weight: bold;
+  margin-top: -65px;
+  color: #0B233C;
+}
+</style>
+"""
+st.markdown(title_styling, unsafe_allow_html=True)
 
-# button = st.markdown("""
-#     <button type="button" class="btn btn-primary">Primary</button>
-# """, unsafe_allow_html=True)
+buttonStyle = """
+    <style>
+        div.stButton > button:first-child {
+            height: 3em;
+            width: 100%; 
+        }
+    </style>
+"""
+st.markdown(buttonStyle, unsafe_allow_html=True)
 
+# __________________ CREATING INPUTS ____________________
+col1, col2 = st.columns(2)
+
+
+with col1:
+    patient_name = st.text_input("Enter your name")
+
+with col2:
+    gender = st.selectbox("Select your gender.", ('Male', 'Female', 'Binary'))
+
+age = st.slider("How old are you?", 0, 130, 35)
+image_file = st.file_uploader("Upload image for detection purposes")
+if image_file is not None:
+    size_mb = image_file.size/(1024**2)
+    file_details = {
+        "filename": image_file.name,
+        "filetype": image_file.type,
+        "filesize": "{:,.2f} MB".format(size_mb)
+    }
+    if file_details['filetype'] in ('image/png', 'imgage/jpg', 'image/jpeg'):
+        st.success("Valid Format!", icon="✅")
+        btnCol1, btnCol2 = st.columns(2)
+        with btnCol1:
+            st.button("Check Results", type="primary")
+        with btnCol2:
+            st.button("Generate Report", type="primary")
+    else:
+        st.error("Invalid Format! Only \'.jpeg\', \'.jpg\', \'.png\' format are accepted", icon="🚨")
